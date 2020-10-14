f7fb38ff473bf6c7b64603b895149dcdf7a7a9ec
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-10-13 21:20:42 -0500

    Funciones declaradas

---
+++ funciones.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func fibonacci (num int ) { //fibonacci recursivo
+
+
+}
+
+func variadic( args ...int) int { //retorna un numero mas grande
+
+
+}
+
+
+
+func main () {
+
+	generaImpares := func() {
+
+	}
+	
+	intercambiar := func (a, b int) {
+	
+	
+	}
+
+
+
+}
